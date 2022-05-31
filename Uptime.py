25e2eba97b9ec22774259c3894c5f1e0c678592f
javajunky <javajunky@gmail.com>
2022-05-31 20:59:24 +0100

    Added online

--- Uptime.py
+++ Uptime.py
@@ -8,13 +8,14 @@ class Uptime(AliceSkill):
 	Description: find out the uptime
 	"""
 	@IntentHandler("Uptime")
-
+	@Online
 	def runUptime(self, session: DialogSession):
 		self.ThreadManager.doLater(interval=0, func=self.getUptime, kwargs={'session': session})
 		self.logInfo("Finding uptime")
 		self.endDialog(sessionID=session.sessionId, text=self.randomTalk('running'))
 
 	@AnyExcept(exceptions=(KeyError), text='failed', printStack=True)
+	@Online
 	def getUptime(self,session: DialogSession):
 			self.logInfo("Calculating uptime.")
 			raw = subprocess.check_output('uptime').decode("utf8").split(" ")
