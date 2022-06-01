114ed468da805069e11460a3ba6787afeb52ec7b
javajunky <javajunky@gmail.com>
2022-06-01 11:22:45 +0100

    changed intent back to uptime.

--- Uptime.py
+++ Uptime.py
@@ -7,7 +7,7 @@ class Uptime(AliceSkill):
 	Author: Bobby C
 	Description: find out the uptime
 	"""
-	@IntentHandler("runUptime")
+	@IntentHandler('Uptime')
 	@Online
 	def runUptime(self, session: DialogSession):
 		self.ThreadManager.doLater(interval=0, func=self.getUptime, kwargs={'session': session})
